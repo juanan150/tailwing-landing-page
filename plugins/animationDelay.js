8a732daf5c507234a9978c897d918acd76cc6cd0
juanan150 <juanan0309@gmail.com>
2023-05-26 09:10:14 -0500

    add hero section with customized plugins and classes

---
+++ plugins/animationDelay.js
@@ -0,0 +1,38 @@
+const plugin = require("tailwindcss/plugin");
+
+const animationDelay = plugin(
+  ({ matchUtilities, theme }) => {
+    matchUtilities({
+      //Allow dynamic values to specific css command
+      "animation-delay": (value) => {
+        //Tailwind expects camelCase, the value is dynamic and is applied to the class name
+        return {
+          animationDelay: value,
+        };
+      },
+    }, {
+      //assign default values to class
+      values: theme("animationDelay"),
+    });
+  },
+  {
+    //add theme with values to specific class, lice animation-delay-100, animation-delay-200 and so on
+    theme: {
+      // className
+      animationDelay: {
+        100: "100ms",
+        200: "200ms",
+        300: "300ms",
+        400: "400ms",
+        500: "500ms",
+        600: "600ms",
+        700: "700ms",
+        800: "800ms",
+        900: "900ms",
+        1000: "1000ms",
+      },
+    },
+  }
+);
+
+module.exports = animationDelay;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {},
-  plugins: [require("./plugins/openVariant")],
+  theme: {
+    extend: {
+      // Keyframes for animation
+      keyframes: {
+        //Keyframe name
+        wavey: {
+          //Animation config
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      //Name will be animation-wavey
+      animation: {
+        //relation to keyframe, css command
+        wavey: "wavey 1000ms linear infinite",
+      },
+    },
+  },
+  plugins: [
+    require("./plugins/openVariant"),
+    require("./plugins/animationDelay"),
+  ],
 };
